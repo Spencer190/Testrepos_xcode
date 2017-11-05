649827a6108bc9dfb90b3fc9b6a7ce97ecb4ad01
Spencer Kranz <skranz190@github.com>
2017-11-05 11:35:03 -0500

    k

--- Testrepos_xcode/FileViewer.h
+++ Testrepos_xcode/FileViewer.h
@@ -1,19 +1,24 @@
 // FileViewer.h
 
+#ifndef _FileViewer_h_
+#define _FileViewer_h_
+
 #include <string>
 
 #include "Buffer.h"
 
 class FileViewer
-{
+{
 public:
-    void run();
-
+    void run();
+    
 private:
     void display();
-    void execute_command(char command, bool & done);
-
-    Buffer buffer_;
-    int window_height_;
-    std::string error_message_;
+    
+    Buffer buffer;
+    int window_height;
+    int line_size;
+    std::string error_message;
 };
+
+#endif
